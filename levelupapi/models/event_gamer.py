5754e54feb18838d20d0938c844c9241caa4d35f
wooldridge12 <wooldridgemikey@gmail.com>
2021-08-03 11:24:44 -0400

    finished book 4 main parts

--- levelupapi/models/event_gamer.py
+++ levelupapi/models/event_gamer.py
@@ -1,6 +1,6 @@
-from django.db import Model 
+from django.db import models
 
 
-class EventGamer():
+class EventGamer(models.Model):
     event = models.ForeignKey("Gamer", on_delete=models.CASCADE)
     gamer = models.ForeignKey("Event", on_delete=models.CASCADE)
