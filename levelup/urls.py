71c5c4f784fcf35ae73c66dc238ae8864dc26b4b
wooldridge12 <wooldridgemikey@gmail.com>
2021-08-04 14:51:01 -0400

    chap 8 finshed

--- levelup/urls.py
+++ levelup/urls.py
@@ -18,11 +18,12 @@ from django.conf.urls import include
 from django.urls import path
 from levelupapi.views import register_user, login_user
 from rest_framework import routers
-from levelupapi.views import GameTypeView, GameView
+from levelupapi.views import GameTypeView, GameView, EventView
 
 router = routers.DefaultRouter(trailing_slash=False)
 router.register(r'gametypes', GameTypeView, 'gametype')
 router.register(r'games', GameView, 'game')
+router.register(r'events', EventView, 'event')
 
 urlpatterns = [
     path('', include(router.urls)),
--- levelupapi/views/__init__.py
+++ levelupapi/views/__init__.py
@@ -2,3 +2,4 @@ from .auth import login_user
 from .auth import register_user
 from .game import Game, GameView
 from .gametype import GameType, GameTypeView
+from .event import Event, EventView
